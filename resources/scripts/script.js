95dd01c362bcc5337a18f821c345457d7337b28c
InOakland <78838820+InOakland@users.noreply.github.com>
2022-04-29 18:32:44 -0700

    added event listeners and corrected eightBall functionality, added prompts to return query and answers

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -3,40 +3,40 @@ const userName = document.getElementById('userName');
 const question = document.getElementById('question');
 
 //Eight-ball functionality
-console.log(`${userName} ask the 8-ball ${question}?`);
 let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = '';
 switch (randomNumber) {
-    case (1):
-        console.log('It is certain');
+    case 1:
+        eightBall = 'It is certain';
         break;
     case 2:
-        console.log('It is decidedly so');
+        eightBall = 'It is decidedly so';
         break;
     case 3:
-        console.log('Reply hazy try again');
+        eightBall = 'Reply hazy try again';
         break;
     case 4:
-        console.log('Cannot predict now');
+        eightBall = 'Cannot predict now';
         break;
     case 5:
-        console.log('Do not count on it');
+        eightBall = 'Do not count on it';
         break;
     case 6:
-        console.log('My sources say no');
+        eightBall = 'My sources say no';
         break;
     case 7:
-        console.log('Outlook not so good');
+        eightBall = 'Outlook not so good';
         break;
     case 0:
-        console.log('Signs point to yes');
+        eightBall = 'Signs point to yes';
         break;
     default:
-        console.log('Please try again');
+        eightBall = 'Please try again';
 }
-
-//Answers 
 
 //Event Listeners
 const submitBut = document.getElementById('submitBut');
-submitBut.onclick = alert('clicked');
+submitBut.onclick = () => {
+    alert(`${userName.value} asked the 8-ball ${question.value}?`);
+    alert(`The magic 8-ball says: ${eightBall}`);
+}
