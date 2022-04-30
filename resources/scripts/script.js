dc1a85c88fddc3f0a848b99b1c2e28f9a3961f57
InOakland <78838820+InOakland@users.noreply.github.com>
2022-04-29 17:37:17 -0700

    added the javascript portion but the elements havent been properly linked so the user sees nothing and the console doesn't respond properly. It is a start though

---
+++ resources/scripts/script.js
@@ -0,0 +1,37 @@
+const userName = document.getElementById('userName');
+const question = document.getElementById('question');
+
+console.log(`${userName} ask the 8-ball ${question}?`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber) {
+    case (1):
+        console.log('It is certain');
+        break;
+    case 2:
+        console.log('It is decidedly so');
+        break;
+    case 3:
+        console.log('Reply hazy try again');
+        break;
+    case 4:
+        console.log('Cannot predict now');
+        break;
+    case 5:
+        console.log('Do not count on it');
+        break;
+    case 6:
+        console.log('My sources say no');
+        break;
+    case 7:
+        console.log('Outlook not so good');
+        break;
+    case 0:
+        console.log('Signs point to yes');
+        break;
+    default:
+        console.log('Please try again');
+}
+
+const submitBut = document.getElementById('submitBut');
+submitBut.onclick = alert('clicked');
